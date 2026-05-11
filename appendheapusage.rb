c4b4c200377934961db6b624bc06457855e20d62
Siarhiej Siamaška <siarhei.siamashka@gmail.com>
2026-05-11 16:07:31 +0300

    dfsd

--- appendheapusage.rb
+++ appendheapusage.rb
@@ -2,9 +2,9 @@
 
 md = File.open(ARGV[0])
 mu = File.open(ARGV[1])
-puts md.gets.strip + " Heap usage |"
+puts md.gets.strip + " Heap usage | Peak RSS |"
 puts md.gets.strip + "---:|"
 while (l = md.gets)
-  abort "incomplete heap usage result\n" unless mu.gets =~ /^Peak heap usage: (\S+)/
-  puts l.strip + " #{$1} |"
+  abort "incomplete heap usage result\n" unless mu.gets =~ /^Heap: (\S+), RSS: (\S+)/
+  puts l.strip + " #{$1} | #{$2} |"
 end
--- heapusage.rb
+++ heapusage.rb
@@ -1,18 +1,28 @@
 #!/usr/bin/env ruby
 
 nuspell = ARGV.include?("-n")
+dumbspell = ARGV.include?("-d")
 args = ARGV.filter {|x| !(x =~ /^\-/) }
 
 dicname  = args[0]
 wordlist = args[1]
 
 cmdline = "hunspell -d #{dicname} -l #{wordlist} 2>/dev/null | grep heaptrack"
+cmdline = "./dumbspell -d #{dicname} -l #{wordlist} 2>/dev/null | grep heaptrack" if dumbspell
 cmdline = "nuspell -d #{dicname} #{wordlist} 2>/dev/null | grep heaptrack" if nuspell
+
+heap = "?"
+rss  = "?"
 
 result = `heaptrack #{cmdline}`
 if result =~ /(heaptrack \-\-analyze.*)/
   result2 = `#{$1.strip}`
   if result2 =~ /peak heap memory consumption\: (\S+)/
-    puts "Peak heap usage: #{$1}"
+    heap = $1
+  end
+  if result2 =~ /peak RSS \(including heaptrack overhead\)\: (\S+)/
+    rss = $1
   end
 end
+
+puts "Heap: #{heap}, RSS: #{rss}"
